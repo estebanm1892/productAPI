940d822b7d3117488475917593c41658847eb50e
estebanm1892 <estebanm1892@gmail.com>
2024-09-17 18:24:26 -0500

    fix gitignore

--- Models/Wishlist.cs
+++
@@ -1,8 +0,0 @@
-public class Wishlist
-{
-    public int Id { get; set; }
-    public int ProductId { get; set; }
-    public Product? Product { get; set; } // Propiedad de navegación, pero no debería inicializarse en el constructor
-    public int UserId { get; set; }
-    public User? User { get; set; } // Propiedad de navegación, pero no debería inicializarse en el constructor
-}
